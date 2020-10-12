5762a1741487d12553c54836282f5072e87a8ffe
Pedro <pheguy.smc@gmail.com>
2020-10-12 18:23:24 -0300

    WIP: working on sql setup

--- api/controllers/machineController.js
+++ api/controllers/machineController.js
@@ -1,5 +1,33 @@
+var sql = require("mssql");
+
+// config for your database
+var config = {
+  user: `${process.env.user}`,
+  password: `${process.env.password}`,
+  server: `${process.env.server}`,
+  database: `${process.env.database}`,
+};
+
+console.log(config);
+
 exports.getAllMachines = function (req, res) {
   res.send("aguante la fafa");
 };
 
-exports.setMachine = function (req, res) {};
+exports.setMachine = function (req, res) {
+  sql.connect(config, function (err) {
+    if (err) console.log(err);
+    // create Request object
+    var request = new sql.Request();
+    // query to the database and get the records
+    request.query(
+      `INSERT INTO machines (Nombre, Instalada, ultimoMantenimiento)
+  VALUES ('Cardinal', 'Erichsen', 'Skagen 21');`,
+      function (err, recordset) {
+        if (err) console.log(err);
+        // send records as a response
+        res.send(recordset);
+      }
+    );
+  });
+};
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
+const sql = require("mssql");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
